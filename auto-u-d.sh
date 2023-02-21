3647d35f8f38563e68dc2a36fa3d1b2a6c44d25b
Manu <emmanuelisai_28@hotmail.com>
2023-02-21 06:13:21 +0000

    update script

--- auto-u-d.sh
+++ auto-u-d.sh
@@ -2,18 +2,18 @@
 
 while :
 do
+	cd /var/www/html
 	git remote update
 	LOCAL=$(git rev-parse @)
 	REMOTE=$(git rev-parse origin/master)
 	BASE=$(git merge-base @ origin/master)
 
 	if [ $LOCAL = $REMOTE ]; then
-		echo "Up-to-date"
-    elif [ $LOCAL = $BASE ]; then
+	    echo "Up-to-date"
+        elif [ $LOCAL = $BASE ]; then
 	    git pull origin master
-
-		#add restart nginx
-		#add start ngrok
-		echo 'updated project'
+	    sudo systemctl reload nginx
+	    ngrok http 80 --log=stdout >/dev/null &
+	    URL=$(curl -sS http://localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url')
 	fi	
 done
